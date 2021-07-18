c8f55bba552f0e797e87bb80f228d73259ecec92
Uluc Ozdenvar <ulucozdenvar01@gmail.com>
2021-07-18 08:06:23 -0500

    domains

--- rowingwebsite2/settings.py
+++ rowingwebsite2/settings.py
@@ -157,7 +157,7 @@ AWS_ACCESS_KEY_ID = os.environ["AWS_ACCESS_KEY_ID"]
 AWS_SECRET_ACCESS_KEY = os.environ["AWS_SECRET_ACCESS_KEY"]
 AWS_STORAGE_BUCKET_NAME = os.environ["AWS_STORAGE_BUCKET_NAME"]
 # AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % os.environ["AWS_STORAGE_BUCKET_NAME"]
-AWS_S3_CUSTOM_DOMAIN = os.environ["CLOUDFLARE_DOMAIN"]
+AWS_S3_CUSTOM_DOMAIN = "https://d19tolcfs26uy5.cloudfront.net"
 AWS_DEFAULT_ACL='public-read'
 AWS_S3_SECURE_URLS=True
 
@@ -175,7 +175,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
 
-STATIC_URL = os.environ["CLOUDFLARE_DOMAIN"]
+STATIC_URL = "https://d19tolcfs26uy5.cloudfront.net"
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 DEFAULT_FILE_STORAGE = 'rowingwebsite2.storages_backend.MediaStorage'
 
